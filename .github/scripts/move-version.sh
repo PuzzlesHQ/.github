d5e1745c4897a15c438a58be617102143d4502b4
Crab_King <127689590+Crab-K1ng@users.noreply.github.com>
2025-04-19 00:27:02 +0100

    add /api/repository/

--- .github/scripts/move-version.sh
+++ .github/scripts/move-version.sh
@@ -62,10 +62,10 @@ echo "Uploading to $DEST_REPO"
 for file in *; do
   echo "→ Uploading $file"
   curl -sSf -X PUT -u "$AUTH" \
-    --data-binary @"$file" "$MAVEN_URL/$DEST_REPO/$ARTIFACT_PATH/$file" || {
+    --data-binary @"$file" "$MAVEN_URL/api/repository/$DEST_REPO/$ARTIFACT_PATH/$file" || {
     echo "Failed to upload $file"
-    echo "$MAVEN_URL/$DEST_REPO/$ARTIFACT_PATH/$file"
-    echo "$MAVEN_URL/$DEST_REPO/$ARTIFACT_PATH"
+    echo "$MAVEN_URL/api/repository/$DEST_REPO/$ARTIFACT_PATH/$file"
+    echo "$MAVEN_URL/api/repository/$DEST_REPO/$ARTIFACT_PATH"
     exit 1
   } 
 done
